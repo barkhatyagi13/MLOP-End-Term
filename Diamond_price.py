2ec7235a99b28b292b01be08b3ae538affc54951
barkhatyagi13 <126963538+barkhatyagi13@users.noreply.github.com>
2023-07-19 11:31:16 +0530

    Add files via upload

--- Diamond_price.py
+++ Diamond_price.py
@@ -58,9 +58,10 @@ def get_user_input():
     return features
 
 # Set the title of the app
+st.markdown('<style>h1{ text-align: center; }</style>', unsafe_allow_html=True)
 st.title('Diamond Price Prediction App')
 st.image("""https://www.thestreet.com/.image/ar_4:3%2Cc_fill%2Ccs_srgb%2Cq_auto:good%2Cw_1200/MTY4NjUwNDYyNTYzNDExNTkx/why-dominion-diamonds-second-trip-to-the-block-may-be-different.png""")
-st.header('Enter the characteristics of the diamond:')
+st.header('Choose characteristics of the diamond:')
 
 # Get user input
 user_input = get_user_input()
